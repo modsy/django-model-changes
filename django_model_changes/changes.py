bc05d371df9130517e2bba5da8effa3e7629c2e7
Ivaylo Stoyanov <ivaylo.stoyanov@modsy.com>
2021-04-28 17:03:14 +0300

    Use only field's attname for state tracking

--- django_model_changes/changes.py
+++ django_model_changes/changes.py
@@ -104,7 +104,6 @@ class ChangesMixin(object):
         Returns a ``field -> value`` dict of the current state of the instance.
         """
         field_names = set()
-        [field_names.add(f.name) for f in self._state_fields()]
         [field_names.add(f.attname) for f in self._state_fields()]
         return dict([(field_name, getattr(self, field_name, None)) for field_name in field_names])
 
